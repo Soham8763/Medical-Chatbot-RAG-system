8180a6e10881a7ba5abd027da4d7a318996b8714
Soham8763 <soham7857@gmail.com>
2025-09-14 17:35:39 +0530

    requirements added for project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot_rag_system",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Soham Ghosh",
+    author_email="soham7857@gmail.com",
+    description="A Medical Chatbot using Retrieval-Augmented Generation (RAG) system",
+)
